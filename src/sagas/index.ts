a93127f968000031aadded8fca9da7c975de915b
Kevin Nathaniel <kevinnathaniel48@gmail.com>
2022-09-22 13:03:38 +0700

    refactor: remove saga

--- src/sagas/index.ts
+++
@@ -1,6 +0,0 @@
-import {all, fork} from 'redux-saga/effects';
-import {watchContactListRequestStart} from './contactList.saga';
-
-export const rootSaga = function* root() {
-  yield all([fork(watchContactListRequestStart)]);
-};
